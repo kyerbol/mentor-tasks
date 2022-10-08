8d714bf4ada5ebd7daaf9037f420c7d368a44aa4
Yerbol <kyryerbol@gmail.com>
2022-10-08 17:38:52 +0600

    init

---
+++ read in JSON/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.Json;
+
+namespace DeserializeFromFileAsync
+{
+    public class WeatherParameters
+    {
+        public DateTimeOffset Date { get; set; }
+        public int Temperature { get; set; }
+        public int WindSpeed { get; set; }
+    }
+
+    public class Program
+    {
+        public static async Task Main()
+        {
+            string fileName = "WeatherParameters.json";
+            using FileStream openStream = File.OpenRead(fileName);
+            WeatherParameters? WeatherParameters =
+                await JsonSerializer.DeserializeAsync<WeatherParameters>(openStream);
+
+            Console.WriteLine($"{WeatherParameters?.Date:M} - {WeatherParameters?.Temperature} deg - {WeatherParameters?.WindSpeed} km/h");
+        }
+    }
+}
