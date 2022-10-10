5ea78e21a49c53a0fea42d4ecd53d2bdbec681b1
Yerbol <kyryerbol@gmail.com>
2022-10-10 10:53:13 +0600

    Deserialize JSON (array) as a stream

--- read in JSON/Program.cs
+++ read in JSON/Program.cs
@@ -1,25 +1,43 @@
 ﻿using System;
 using System.Text.Json;
+using System.Collections;
+using WeatherParameter;
+using System.IO;
+
 
 namespace DeserializeFromFileAsync
 {
-    public class WeatherParameters
-    {
-        public DateTimeOffset Date { get; set; }
-        public int Temperature { get; set; }
-        public int WindSpeed { get; set; }
-    }
+
 
     public class Program
     {
         public static async Task Main()
         {
+            // 1-option with using System.Text.Json;
+
             string fileName = "WeatherParameters.json";
             using FileStream openStream = File.OpenRead(fileName);
-            WeatherParameters? WeatherParameters =
-                await JsonSerializer.DeserializeAsync<WeatherParameters>(openStream);
 
-            Console.WriteLine($"{WeatherParameters?.Date:M} - {WeatherParameters?.Temperature} deg - {WeatherParameters?.WindSpeed} km/h");
+            await foreach (var item in JsonSerializer.DeserializeAsyncEnumerable<WeatherParameters>(openStream))
+            {
+                Console.WriteLine($"{item?.Date:M} - {item?.Temperature} deg - {item?.WindSpeed} km/h");
+            }
+
+            // 2-option with using Newtonsoft.Json;
+            /*
+            string json = System.IO.File.ReadAllText("WeatherParameters.json");
+            var people = Newtonsoft.Json.JsonConvert.DeserializeObject<List<WeatherParameters>>(json);
+            if (people is not null)
+                foreach (var item in people)
+            {
+                Console.WriteLine($"{item?.Date:M} - {item?.Temperature} deg - {item?.WindSpeed} km/h");
+            }
+            */
+
         }
+
     }
 }
+
+
+
