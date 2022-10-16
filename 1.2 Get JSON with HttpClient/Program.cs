ead347f0eed8d0e889c56a66e058a4db7087ed57
Yerbol <kyryerbol@gmail.com>
2022-10-16 13:16:18 +0600

    Parsing JSON with HttpClient

--- 1.2 Get JSON with HttpClient/Program.cs
+++ 1.2 Get JSON with HttpClient/Program.cs
@@ -1,7 +1,7 @@
-﻿//using Newtonsoft.Json;
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Net.Http.Json;
@@ -9,6 +9,8 @@ using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using WeatherMapParse;
+using System.IO;
+using System.Linq;
 
 namespace WebAPIClient
 {
@@ -20,26 +22,41 @@ namespace WebAPIClient
         {
             var WeatherParametrs = await GetWeather();
 
-            foreach (var repo in WeatherParametrs)
+            Console.WriteLine(WeatherParametrs.cod);
+            Console.WriteLine(WeatherParametrs.message);
+            Console.WriteLine(WeatherParametrs.cnt);
+           
+            foreach (var item in WeatherParametrs.lists)
             {
-                Console.WriteLine(repo.cod);
-                // Console.WriteLine(repo.message);
-                /*   Console.WriteLine(repo.cnt);
-                   */
+                Console.WriteLine(item.dt);
+
+                foreach (var itemWeaher in item.weather)
+                {
+                    Console.WriteLine(itemWeaher.Main);
+                    Console.WriteLine(itemWeaher.Description);
+                }
+                    
+                Console.WriteLine(item.Main.temp);
+                Console.WriteLine(item.Main.temp_min);
+                Console.WriteLine(item.Main.temp_max);
             }
+             
+            Console.WriteLine(WeatherParametrs.city.name, WeatherParametrs.city.country);
+            
         }
 
-        private static async Task<List<WeatherMapResponse>> GetWeather()
+        private static async Task<WeatherMapResponse> GetWeather()
         {
-             client.DefaultRequestHeaders.Accept.Clear();
-             client.DefaultRequestHeaders.Accept.Add(
-                    new MediaTypeWithQualityHeaderValue("application/json"));
-             client.DefaultRequestHeaders.Add("User-Agent", "Csharp");
-            HttpClient httpclient = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            client.DefaultRequestHeaders.Add("User-Agent", "Chrome");
             var streamTask = client.GetStreamAsync("https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=c01731d6b51bc49477ef31e86290b79c");
-            var WeatherParametrs = await JsonSerializer.DeserializeAsync<List<WeatherMapResponse>>(await streamTask);
+            var WeatherParametrs = await JsonSerializer.DeserializeAsync<WeatherMapResponse>(await streamTask);
                        
             return WeatherParametrs;
         }
     }
 }
+
+
+
