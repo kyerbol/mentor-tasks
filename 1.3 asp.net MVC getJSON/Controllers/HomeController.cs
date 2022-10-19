ff1187e2ed052674153fbc07154aab300619aa9d
Yerbol <kyryerbol@gmail.com>
2022-10-19 18:49:26 +0600

    aspnet MVC getJSON

---
+++ 1.3 asp.net MVC getJSON/Controllers/HomeController.cs
@@ -0,0 +1,78 @@
+﻿using GetJSON.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Text.Json;
+using System.Net.Http.Headers;
+
+namespace GetJSON.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private static readonly HttpClient client = new HttpClient();
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        private static async Task<WeatherMapResponse> GetWeather()
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            client.DefaultRequestHeaders.Add("User-Agent", "Chrome");
+            var streamTask = client.GetStreamAsync("https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=c01731d6b51bc49477ef31e86290b79c");
+            var WeatherParametrs = await JsonSerializer.DeserializeAsync<WeatherMapResponse>(await streamTask);
+
+            return WeatherParametrs;
+        }
+      
+        public ActionResult Weather()
+        {
+
+            return View();
+        }
+
+        public async Task<IActionResult> Index()
+        {
+
+
+            var WeatherParametrs = await GetWeather();
+
+          //  var dr = WeatherParametrs.cod;
+           /* Console.WriteLine(WeatherParametrs.cod);
+            Console.WriteLine(WeatherParametrs.message);
+            Console.WriteLine(WeatherParametrs.cnt);
+
+            foreach (var item in WeatherParametrs.lists)
+            {
+                Console.WriteLine(item.dt);
+
+                foreach (var itemWeaher in item.weather)
+                {
+                    Console.WriteLine(itemWeaher.Main);
+                    Console.WriteLine(itemWeaher.Description);
+                }
+
+                Console.WriteLine(item.Main.temp);
+                Console.WriteLine(item.Main.temp_min);
+                Console.WriteLine(item.Main.temp_max);
+            }
+
+            Console.WriteLine(WeatherParametrs.city.name, WeatherParametrs.city.country);*/
+
+            return View(WeatherParametrs);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ 1.3 asp.net MVC getJSON/Models/WeatherMapParse.cs
@@ -0,0 +1,86 @@
+﻿using System.Text.Json.Serialization;
+
+namespace GetJSON.Models
+{
+    public class WeatherMapResponse
+    {
+        public string cod { get; set; }
+        public int message { get; set; }
+        public int cnt { get; set; }
+        [JsonPropertyName("list")]
+        public Lists[] lists { get; set; }
+        public City city { get; set; }
+    }
+
+    public class City
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public Coord coord { get; set; }
+        public string country { get; set; }
+        public int population { get; set; }
+        public int timezone { get; set; }
+        public int sunrise { get; set; }
+        public int sunset { get; set; }
+    }
+
+    public class Coord
+    {
+        public float lat { get; set; }
+        public float lon { get; set; }
+    }
+
+    public class Lists
+    {
+        public int dt { get; set; }
+        [JsonPropertyName("main")]
+        public Main Main { get; set; }
+        public Weather[] weather { get; set; }
+        public Clouds clouds { get; set; }
+        public Wind wind { get; set; }
+        public int visibility { get; set; }
+        public float pop { get; set; }
+        public Sys sys { get; set; }
+        public string dt_txt { get; set; }
+    }
+
+    public class Main
+    {
+        public float temp { get; set; }
+        public float feels_like { get; set; }
+        public float temp_min { get; set; }
+        public float temp_max { get; set; }
+        public int pressure { get; set; }
+        public int sea_level { get; set; }
+        public int grnd_level { get; set; }
+        public int humidity { get; set; }
+        public float temp_kf { get; set; }
+    }
+
+    public class Clouds
+    {
+        public int all { get; set; }
+    }
+
+    public class Wind
+    {
+        public float speed { get; set; }
+        public int deg { get; set; }
+        public float gust { get; set; }
+    }
+
+    public class Sys
+    {
+        public string pod { get; set; }
+    }
+
+    public class Weather
+    {
+        public int id { get; set; }
+        [JsonPropertyName("main")]
+        public string Main { get; set; }
+        [JsonPropertyName("Description")]
+        public string Description { get; set; }
+        public string icon { get; set; }
+    }
+}
